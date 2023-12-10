851470ea6b65bc2d016f1754a7072258220fd30a
Rizki <106717697+RizqGyx@users.noreply.github.com>
2023-12-10 15:45:26 +0700

    Adding Zombie
    Need Throwable(Grenade) Script To Implement on Grenade

--- Assets/Scripts/Enemy/Zombie.cs
+++ Assets/Scripts/Enemy/Zombie.cs
@@ -7,9 +7,12 @@ public class Zombie : MonoBehaviour
     [SerializeField] private int HP = 100;
     private Animator animator;
 
+    private UnityEngine.AI.NavMeshAgent navAgent;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
+        navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
     public void TakeDamage(int damageAmount)
@@ -18,12 +21,35 @@ public class Zombie : MonoBehaviour
 
         if(HP <= 0)
         {
-            animator.SetTrigger("DIE");
-            Destroy(gameObject);
+            int randomValue = Random.Range(0,3);
+
+            if(randomValue == 0)
+            {
+                animator.SetTrigger("DIE1");
+            }
+            else if (randomValue == 1)
+            {
+                animator.SetTrigger("DIE2");
+            }
+            else
+            {
+                animator.SetTrigger("DIE3");
+            }
         }
         else
         {
             animator.SetTrigger("DAMAGE");
+        }
+    }
+
+    private void Update()
+    {
+        if(navAgent.velocity.magnitude > 0.1f)
+        {
+            animator.SetBool("isWalking", true);
+        }
+        else {
+            animator.SetBool("isWalking", false);
         }
     }
 }
