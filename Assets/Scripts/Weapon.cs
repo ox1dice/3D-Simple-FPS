fe11506010b8e9c72f0821bf9921b12f18bec9af
Daffa Nurrizqi <ndaffaraihan@gmail.com>
2023-12-10 13:09:25 +0700

    Added Weapon script and AI Navigation package

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    public float bulletVelocity = 30;
+    public float bulletPrefabLifeTime = 3f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        throw new NotImplementedException();
+    }
+}
